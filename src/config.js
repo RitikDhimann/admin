b95a3e912affee0379e0b963eb60f5e0804c57a2
Ritik Dhiman <rdhiman0744@gmail.com>
2026-04-20 13:35:49 +0530

    changes

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 const isLocalhost = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
 export const API_BASE = isLocalhost 
-  ? 'http://localhost:3043/api' 
+  ? 'http://localhost:5000/api' 
    : process.env.REACT_APP_API_BASE;
   
   // : 'https://apisurprise.dodunsoftsolutions.com/api';
 
 export const BASE_URL = isLocalhost 
-  ? 'http://localhost:3043' 
+  ? 'http://localhost:5000' 
   : 'https://apisurprise.dodunsoftsolutions.com';
